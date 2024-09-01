0c52cad636c0fa8c407973cf9bc41dd968adad1d
Manav1011 <manavshah1011.ms@gmail.com>
2024-09-01 09:52:09 +0530

    i've written minimum code for the test cases to be passed successufully for the add_book feature add added the output to add_book_output.log file which means all of the test from add_book feature have works according to the rule 2 of TDD

--- add_book_test.py
+++ add_book_test.py
@@ -10,55 +10,58 @@ class TestMultiplyFunction(unittest.TestCase):
         self.LMS = LibraryManagement()
 
     def test_return_type_is_boolean(self):
-        self.result = self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
+        self.result = self.LMS.add_book(1,'Test Book','Test Author',2024)
         self.assertIsInstance(self.result,bool,"The return type of add_book should be a boolean only!!")
     
     def test_check_for_null_values(self):
         # I'm simply raising an assertion error when any of the input parameter is null or not defined
-        with self.assertRaises(Exception) as e:
-            self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
-        self.assertEqual(str(e.exception),'parameters_missing')
+        self.assertRaises(Exception, self.LMS.add_book,None,'Test Book','Test Author',2024)
+        self.assertRaises(Exception, self.LMS.add_book,1,None,'Test Author',2024)
+        self.assertRaises(Exception, self.LMS.add_book,1,"Test Book",None,2024)
+        self.assertRaises(Exception, self.LMS.add_book,1,'Test Book','Test Author',None)
+        self.assertRaises(Exception, self.LMS.add_book,None,None,None,None)
+        self.assertRaises(Exception, self.LMS.add_book)
 
     def test_add_book_string_ISBN(self):
         # I'm simply raising an assertion error  when ISBN is a stirng type
         with self.assertRaises(Exception) as e:
-            self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
+            self.LMS.add_book("1",'Test Book','Test Author',2024)
         self.assertEqual(str(e.exception),'type_mismatch_for_isbn')
 
     def test_add_book_string_title(self):
         # I'm simply raising an assertion error  when title is a stirng type
         with self.assertRaises(Exception) as e:
-            self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
+            self.LMS.add_book(1,bool,'Test Author',2024)
         self.assertEqual(str(e.exception),'type_mismatch_for_title')
     
     def test_add_book_string_author(self):
         # I'm simply raising an assertion error  when author is a stirng type
         with self.assertRaises(Exception) as e:
-            self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
+            self.LMS.add_book(1,'Test Book',dict,2024)
         self.assertEqual(str(e.exception),'type_mismatch_for_author')
 
     def test_add_book_int_publication_year(self):
         # I'm simply raising an assertion error  when publication_year is a int type
         with self.assertRaises(Exception) as e:
-            self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
+            self.LMS.add_book(1,'Test Book','Test Author',"2024")
         self.assertEqual(str(e.exception),'type_mismatch_for_publication_year')
     
     def test_future_publication_year(self):
         # I'm simply raising an assertion error  when publication_year is either current year or a future year        
         with self.assertRaises(Exception) as e:
-            self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
+            self.LMS.add_book(1,'Test Book','Test Author',2026)
         self.assertEqual(str(e.exception),'wrong_publication_year')
 
     def test_minimum_title_length(self):
         # I'm simply raising an assertion error  when title_length is less then 2 characters
         with self.assertRaises(Exception) as e:
-            self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
-        self.assertEqual(str(e.exception),'title_lenght_too_low')
+            self.LMS.add_book(1,'T','Test Author',2024)
+        self.assertEqual(str(e.exception),'title_length_too_low')
     
     def test_special_characters_in_title(self):
         # I'm simply raising an assertion error  when there are any special characters in the title
         with self.assertRaises(Exception) as e:
-            self.LMS.add_book('dummy-isbn','Test Book','Test Author',2024)
+            self.LMS.add_book(1,'Test $ Book','Test Author',2024)
         self.assertEqual(str(e.exception),'special_characters_are_not_allowed')
 
 
--- library_management.py
+++ library_management.py
@@ -1,4 +1,6 @@
 import sqlite3
+from datetime import datetime
+import re
 
 class LibraryManagement():
     def __init__(self):
@@ -6,7 +8,41 @@ class LibraryManagement():
         self.cursor = self.conn.cursor()
 
     def add_book(self,isbn,title,author,publication_year):
-        pass
+        
+        # for the test case test_check_for_null_values
+        if isbn is None or title is None or author is None or publication_year is None:
+            raise Exception('parameters_missing')        
+        
+        # for the test_add_book_string_ISBN
+        if not isinstance(isbn, int):
+            raise Exception('type_mismatch_for_isbn')
+
+        # For the test_add_book_string_title
+        if not isinstance(title, str):
+            raise Exception('type_mismatch_for_title')
+
+        # For the test_add_book_string_author
+        if not isinstance(author, str):
+            raise Exception('type_mismatch_for_author')
+
+        # For the test_add_book_int_publication_year
+        if not isinstance(publication_year, int):
+            raise Exception('type_mismatch_for_publication_year')
+
+        # for the test_future_publication_year
+        current_year = datetime.now().year
+        if publication_year > current_year:
+            raise Exception('wrong_publication_year')
+
+        # For the test_minimum_title_length
+        if len(title) < 2:
+            raise Exception('title_length_too_low')
+
+        # For the test_special_characters_in_title
+        if re.search(r'[^a-zA-Z0-9\s]', title):
+            raise Exception('special_characters_are_not_allowed')
+    
+        return True
 
     def borrow_book(self,isbn):
         self.cursor.execute(f'select isbn from book where isbn="{isbn}"')
